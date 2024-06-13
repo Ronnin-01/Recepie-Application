06e594210bb7570749a4e757200b54a2a2be073a
Ronnin-01 <omarmd425@gmail.com>
2024-06-13 14:30:13 +0530

    dimens created

--- AndroidStudioProjects/Cookbook/app/src/main/java/com/bldsht/cookbook/bottomsheet/MealBottomSheetFragment.kt
+++ AndroidStudioProjects/Cookbook/app/src/main/java/com/bldsht/cookbook/bottomsheet/MealBottomSheetFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bldsht.cookbook.R
-import com.bldsht.cookbook.databinding.FragmentCategoryBinding
 import com.bldsht.cookbook.databinding.FragmentMealBottomSheetBinding
 
+private const val MEAL_ID = "param1"
 class MealBottomSheetFragment : Fragment() {
+    private var mealId: String? = null
 
     private var _binding: FragmentMealBottomSheetBinding? = null
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            mealId = it.getString(MEAL_ID)
+        }
 
     }
 
@@ -28,9 +31,22 @@ class MealBottomSheetFragment : Fragment() {
 
         return root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
 
+    companion object {
+        fun newInstance(param1: String) =
+            MealBottomSheetFragment().apply {
+                arguments = Bundle().apply {
+                    putString(MEAL_ID, param1)
+                }
+            }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
