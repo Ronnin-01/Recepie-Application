f1a1f0be11fba897f233982c72a2b4f5bd2bf814
Ronnin-01 <omarmd425@gmail.com>
2024-06-16 01:42:57 +0530

    application is completed

--- AndroidStudioProjects/Cookbook/app/src/main/java/com/bldsht/cookbook/viewmodel/HomeViewModel.kt
+++ AndroidStudioProjects/Cookbook/app/src/main/java/com/bldsht/cookbook/viewmodel/HomeViewModel.kt
@@ -27,12 +27,19 @@ class HomeViewModel(private val mealDataBase: MealDatabase): ViewModel() {
     private var bottomSheetMealLiveData = MutableLiveData<Meal>()
     private val searchMealLiveData = MutableLiveData<List<Meal>>()
 
-    fun getRandomMeal(){
+
+    private var saveStateRandomMeal : Meal? = null
+    fun getRandomMeal() {
+        saveStateRandomMeal?.let { randomMeal ->
+            randomMealLiveData.postValue(randomMeal)
+            return
+        }
         RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
                 if (response.body() != null) {
                     val randomMeal: Meal = response.body()!!.meals[0]
                     randomMealLiveData.value = randomMeal
+                    saveStateRandomMeal = randomMeal
                 } else {
                     return
                 }
